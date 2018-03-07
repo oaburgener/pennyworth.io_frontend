ea6ad9191c29316ae34d6377f04bc01a0baedf16
Ashley Amato <ashleyamato1@gmail.com>
2018-03-07 16:51:04 -0700

    width change on text inputs and buttons

--- frontend/penny_app/Components/Login.js
+++ frontend/penny_app/Components/Login.js
@@ -22,7 +22,7 @@ class Login extends Component {
           <Text style={styles.loginTextStyles}>Login</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.submitButtonStyle}>
-          <Text style={styles.submitTextStyles}>Submit</Text>
+          <Text style={styles.submitTextStyles}>Sign Up</Text>
         </TouchableOpacity>
       </View>
 
@@ -40,8 +40,9 @@ const styles = {
     height: 60,
     alignSelf: 'stretch',
     marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
+    marginLeft: 40,
+    marginRight: 40,
+    paddingLeft: 10,
     borderRadius: 5,
     borderWidth: 1.5,
     borderColor: '#982D37'
@@ -49,7 +50,7 @@ const styles = {
   emailTextStyles: {
     alignSelf: 'flex-start',
     marginTop: 50,
-    marginLeft: 10,
+    marginLeft: 40,
     color: '#982D37',
     fontSize: 16,
     fontWeight: '600'
@@ -57,45 +58,45 @@ const styles = {
   passwordTextStyles: {
     alignSelf: 'flex-start',
     marginTop: 20,
-    marginLeft: 10,
+    marginLeft: 40,
     color: '#982D37',
     fontSize: 16,
-    fontWeight: '600'
+    fontWeight: '600',
   },
   loginTextStyles: {
     alignSelf: 'center',
     color: '#982D37',
     fontSize: 16,
     fontWeight: '600',
-    paddingTop: 10,
-    paddingBottom: 10
+    paddingTop: 15,
+    paddingBottom: 15
   },
   submitTextStyles: {
     alignSelf: 'center',
     color: '#fff',
     fontSize: 16,
     fontWeight: '600',
-    paddingTop: 10,
-    paddingBottom: 10
+    paddingTop: 15,
+    paddingBottom: 15
   },
   loginButtonStyle: {
     alignSelf: 'stretch',
     backgroundColor: '#fff',
-    borderRadius: 5,
+    borderRadius: 7,
     borderWidth: 1.5,
     borderColor: '#982D37',
-    marginLeft: 10,
-    marginRight: 10,
+    marginLeft: 40,
+    marginRight: 40,
     marginTop: 50
   },
   submitButtonStyle: {
     alignSelf: 'stretch',
     backgroundColor: '#982D37',
-    borderRadius: 5,
+    borderRadius: 7,
     borderWidth: 1,
     borderColor: '#982D37',
-    marginLeft: 10,
-    marginRight: 10,
+    marginLeft: 40,
+    marginRight: 40,
     marginTop: 10
   }
 }
