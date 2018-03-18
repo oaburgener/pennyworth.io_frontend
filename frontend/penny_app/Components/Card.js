db67844ac6e42654b467adcad06ea5ad2cb8f863
Ashley Amato <ashleyamato1@gmail.com>
2018-03-18 17:48:13 -0600

    date reformat on cards

--- frontend/penny_app/Components/Card.js
+++ frontend/penny_app/Components/Card.js
@@ -7,15 +7,25 @@ class Card extends Component {
     this.props.getUserServices()
   }
 
+  formatMonth(date) {
+    let month = ['Jan.', 'Feb.', 'March', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.']
+    for (var i = 0; i < month.length; i++) {
+      if (date.slice(5, 7) == i) {
+      return `${month[i -1]}, ${date.slice(8, 10)}`
+      }
+    }
+  }
+
   render () {
 
     return (
       <ScrollView>
         {this.props.userServices && this.props.userServices.map(service => {
+
           return(
             <View key={service.id} style={styles.containerStyle}>
               <Text style={styles.paragraphStyle}>{"Upcoming"}</Text>
-              <Text style={styles.headerStyle}>{service.date}</Text>
+              <Text style={styles.headerStyle}>{this.formatMonth(service.date)}</Text>
               <View style={styles.pennyworkerContainer}>
                 <Image style={styles.pennyworkerImage} source={require('../assets/nate.jpg')} />
                 <Text style={styles.pennyworkerStyle}>{"name"}</Text>
@@ -51,6 +61,7 @@ const styles = {
     color: '#A1A1A5',
     fontSize: 15,
     fontWeight: 'bold',
+    paddingBottom: 10,
     padding: 5,
   },
   pennyworkerStyle: {
